f6a00d927be107bc6931fb178618e0b2259a21a5
Mihaela <Mihaela@DESKTOP-7OJTDVJ>
2025-05-06 13:41:18 +0300

    structura Telefon - modificare functie afisare

--- Telefon.c
+++ Telefon.c
@@ -22,12 +22,8 @@ struct Telefon initializare(int id, int ram, const char* producator, float pret,
 
 //afisarea tuturor atributelor.
 void afisare(struct Telefon t) {
-	if (t.producator != NULL) {
-		printf("%d. Telefonul %s seria %c are %d Gb RAM, costa %5.2f RON\n", t.id, t.producator, t.serie, t.ram, t.pret);
-	}
-	else {
-		printf("%d. Telefonul No Name seria %c are %d Gb RAM, costa %5.2f RON\n", t.id, t.serie, t.ram, t.pret);
-	}
+	// varianta comasata
+	printf("%d. Telefonul %s seria %c are %d Gb RAM, costa %5.2f RON\n", t.id, (t.producator != NULL) ? t.producator : "No Name", t.serie, t.ram, t.pret);
 }
 
 //modificare atribut pret, este un fel de setter
